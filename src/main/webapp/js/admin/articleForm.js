a8dc1b6a9bfd707dc6e9a3e3f01041fad21c103a
Cheetah <murphe@qq.com>
2021-02-16 14:55:51 +0800

    common update for sql file

--- src/main/webapp/js/admin/articleForm.js
+++ src/main/webapp/js/admin/articleForm.js
@@ -1,36 +1,36 @@
-layui.use(['form', 'layer'], function(){
+layui.use(['form', 'layer'], function () {
     var form = layui.form
-        ,layer = layui.layer;
+        , layer = layui.layer;
 
-    var editor = CKEDITOR.replace( 'content' );
+    var editor = CKEDITOR.replace('content');
 
-    form.on('submit(publishArticle)', function(data){
+    form.on('submit(publishArticle)', function (data) {
         var content = editor.getData();
-        if(!content){
+        if (!content) {
             layer.alert("文章内容不能为空", {icon: 2});
             return false;
         }
-        var isStick = 0,allowComment = 0;
-        if(data.field.isStick == "on"){
+        var isStick = 0, allowComment = 0;
+        if (data.field.isStick == "on") {
             isStick = 1;
         }
-        if(data.field.allowComment == "on"){
+        if (data.field.allowComment == "on") {
             allowComment = 1;
         }
         $.ajax({
             url: "admin/publishArticle",
             data: {
                 id: data.field.articleId,
-                categoryId:data.field.categoryId,
-                label:data.field.label,
-                title:data.field.title,
-                allowComment:allowComment,
-                isStick:isStick,
-                content:editor.getData()
+                categoryId: data.field.categoryId,
+                label: data.field.label,
+                title: data.field.title,
+                allowComment: allowComment,
+                isStick: isStick,
+                content: editor.getData()
             },
             type: "POST",
-            success: function(response){
-                if(response.errNo == 0){
+            success: function (response) {
+                if (response.errNo == 0) {
                     //layer.alert(response.errMsg, {icon: 1});
                     layer.alert(response.errMsg, {
                         icon: 1, yes: function () {
@@ -38,8 +38,8 @@ layui.use(['form', 'layer'], function(){
                             parent.layer.closeAll('iframe'); //再执行关闭
                         }
                     })
-                }else {
-                    layer.alert(response.errMsg == undefined? response:"ERROR", {icon: 2});
+                } else {
+                    layer.alert(response.errMsg == undefined ? response : "ERROR", {icon: 2});
                 }
             },
             error: function () {
